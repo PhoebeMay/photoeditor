0e31f7e67055ce30864e779137c6df2cbba414ac
V5787 (Course ID) <v5787@cam.ac.uk>
2015-08-13 12:17:17 +0100

    some filters

--- module1.js
+++ module1.js
@@ -28,12 +28,114 @@ function getRandomInt(min, max) {
 
 // function definitions here
 
+function makeMoreBlue(pixels, adjust) {
+
+    //var pixels = ImageUtils.getPixels(img);
+    var length = pixels.data.length;
+    var data = pixels.data;
+
+    for (var i = 0; i < length; i += 4) {
+        data[i+2] = data[i+2] + adjust;
+    }
+    //ImageUtils.putPixels(pixels, img.width, img.height);
+}
+
+function makeMoreRed(pixels, adjust) {
+
+    //var pixels = ImageUtils.getPixels(img);
+    var length = pixels.data.length;
+    var data = pixels.data;
+
+    for (var i = 0; i < length; i += 4) {
+        data[i] = data[i] + adjust;
+    }
+    //ImageUtils.putPixels(pixels, img.width, img.height);
+}
+
+function makeMoreGreen(pixels, adjust) {
+
+    //var pixels = ImageUtils.getPixels(img);
+    var length = pixels.data.length;
+    var data = pixels.data;
+
+    for (var i = 0; i < length; i += 4) {
+        data[i+1] = data[i+1] + adjust;
+    }
+    //ImageUtils.putPixels(pixels, img.width, img.height);
+}
+
+function makeInvert(pixels, adjust) {
+        //var pixels = ImageUtils.getPixels(img);
+        var length = pixels.data.length;
+        var data = pixels.data;
+
+
+        for (var i = 0; i < length; i += 4) {
+            data[i] = 255 - data[i] ;
+            data[i+1] = 255 -data[i+1];
+            data[i+2] = 255 -data[i+2] ;
+        }
+        //ImageUtils.putPixels(pixels, img.width, img.height);
+}
+
+function brighten(img, adjust) {
+    //var pixels = ImageUtils.getPixels(img);
+    var length = pixels.data.length;
+    var data = pixels.data;
+
+
+    for (var i = 0; i < length; i += 4) {
+        data[i] = data[i] + adjust;
+        data[i+1] = data[i+1] + adjust;
+        data[i+2] = data[i+2] + adjust;
+    }
+    //ImageUtils.putPixels(pixels, img.width, img.height);
+
+}
+
+function noise(pixels) {
+    //var pixels = ImageUtils.getPixels(img);
+    var length = pixels.data.length;
+    var data = pixels.data;
+
+
+    for (var i = 0; i < length; i += 4) {
+        data[i] = data[i] + getRandomInt(-50, 50);
+        data[i+1] = data[i+1] + getRandomInt(-50, 50);
+        data[i+2] = data[i+2] + getRandomInt(-50, 50);
+    }
+    //ImageUtils.putPixels(pixels, img.width, img.height);
+}
+
+function makeFunky(pixels) {
+    //var pixels = ImageUtils.getPixels(img);
+    var length = pixels.data.length;
+    var data = pixels.data;
+
+    for (var i = 0; i < length/2; i += 2) {
+        var temp = data[i];
+        data[i] = data[length - i];
+        data[length - i] = temp;
+    }
+    //ImageUtils.putPixels(pixels, img.width, img.height);
+}
+
 
 $(document).ready(function() {
     var img = new Image();
-    img.src = "img/cat.jpg";
+    img.src = "img/kermit.jpg";
+    var pixels = ImageUtils.getPixels(img);
+    makeFunky(pixels);
+    makeMoreRed(pixels,-255);
+    //var pixels = ImageUtils.getPixels(img);
+    //var heightPixelsToHide = 100;
+    ////console.log(pixels);
+    //
+    //for(var i = 0; i < img.width * heightPixelsToHide * 4; i++) {
+    //    pixels.data[i] = 255;
+    //}
 
-
-
-
+    ImageUtils.putPixels(pixels, img.width, img.height);
 });
+
+//pixels[(i * pixels.width + j) * 4]
--- module2.js
+++ module2.js
@@ -22,17 +22,113 @@ class ImageUtils {
 
 }
 
+class RGBA {
+    constructor(redValue, greenValue, blueValue, alphaValue){
+        this.red = redValue;
+        this.green = greenValue;
+        this.blue = blueValue;
+        this.alpha = alphaValue;
+    }
+
+}
+
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// function definitions here
+function colourise(pixels, colour, level) {
+
+    var all = pixels.data.length;
+    var data = pixels.data;
+
+    for (var i = 0; i < all; i += 4) {
+        var origRGBA = new RGBA(data[i], data[i+1], data[i+2], data[i+3]);
+
+        var modifiedRGBA = colourisePixel(origRGBA, colour, level);
+        setPixel(data, i, modifiedRGBA);
+    }
+}
+
+function colourisePixel(originalRGBA, colour, level) {
+
+    var diffRed = (originalRGBA.red - colour.red) * (level / 100);
+    var modifiedRed = originalRGBA.red - diffRed;
+
+    var diffGreen = (originalRGBA.green - colour.green) * (level / 100);
+    var modifiedGreen = originalRGBA.green - diffGreen;
+
+    var diffBlue = (originalRGBA.blue - colour.blue) * (level / 100);
+    var modifiedBlue = originalRGBA.blue - diffBlue;
+
+    return new RGBA(modifiedRed, modifiedGreen, modifiedBlue, colour.alpha);
+}
+
+function clip(pixels, range){
+        var all = pixels.data.length;
+        var data = pixels.data;
+
+        for (var i = 0; i < all; i += 4) {
+            var origRGBA = new RGBA(data[i], data[i+1], data[i+2], data[i+3]);
+            var modifiedRGBA = clipPixel(origRGBA, range);
+            //console.log(modifiedRGBA);
+            setPixel(data, i, modifiedRGBA);
+        }
+
+}
+
+function clipPixel(colour, range) {
+
+    var clippedRed = 0;
+    var clippedGreen = 0;
+    var clippedBlue = 0;
+
+    if(colour.red > 255 - range) {
+        clippedRed = 255;
+    }
+
+    if(colour.green > 255 - range) {
+        clippedGreen = 255;
+    }
+
+    if(colour.blue > 255 - range) {
+        clippedBlue = 255;
+    }
+
+    return new RGBA(clippedRed, clippedGreen, clippedBlue, colour.alpha);
+
+}
+
+function noise(pixels) {
+    //var pixels = ImageUtils.getPixels(img);
+    var length = pixels.data.length;
+    var data = pixels.data;
+
+
+    for (var i = 0; i < length; i += 4) {
+        data[i] = data[i] + getRandomInt(-50, 50);
+        data[i+1] = data[i+1] + getRandomInt(-50, 50);
+        data[i+2] = data[i+2] + getRandomInt(-50, 50);
+    }
+    //ImageUtils.putPixels(pixels, img.width, img.height);
+}
+
+
+function setPixel(data, i, colour) {
+    data[i] = colour.red;
+    data[i+1] = colour.green;
+    data[i+2] = colour.blue;
+    data[i+3] = colour.alpha;
+}
+
+var colour = new RGBA(59, 55,22,255);
 
 $(document).ready(function() {
     var img = new Image();
-    img.src = "img/cat.jpg";
+    img.src = "img/kermit.jpg";
+    var pixels = ImageUtils.getPixels(img);
 
+    noise(pixels);
 
-
-
+    console.log(pixels);
+    ImageUtils.putPixels(pixels, img.width, img.height);
 });
